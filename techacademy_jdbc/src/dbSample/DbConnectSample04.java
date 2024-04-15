913997f0dcd93018aa7a08e772a7657f847101bc
dir-FY24onoyuto <yuto.ono.t380@dirbato.co.jp>
2024-04-15 12:11:26 +0900

    コミット

--- techacademy_jdbc/src/dbSample/DbConnectSample04.java
+++ techacademy_jdbc/src/dbSample/DbConnectSample04.java
@@ -9,7 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DbConnectSample03 {
+public class DbConnectSample04 {
 
     public static void main(String[] args) {
         
